7d7f210ec42d73d5482aef8e17a6705320739156
Stefan Dudley <59715788+Dudlst01@users.noreply.github.com>
2023-04-17 23:48:17 -0400

    Update 5_Peer_Graded_Assignment_Questions.py
    Still need to fix syntax error at line 151

--- 5_Peer_Graded_Assignment_Questions.py
+++ 5_Peer_Graded_Assignment_Questions.py
@@ -71,9 +71,9 @@ def compute_data_choice_2(df):
 
 
 # Application layout
-app.layout = html.Div(children=[html.H1('US Domestic Airline Flights Performance')],
+app.layout = html.Div(children=[ html.H1('US Domestic Airline Flights Performance')],
                                 # TASK1: Add title to the dashboard
-                                # Enter your code below. Make sure you have correct formatting.
+                                # Enter your code below. Make sure you have correct formatting. 
                                 style={'textAlign': 'left', 'color': '#503D36', 'font-size': 24},
                                 # REVIEW2: Dropdown creation
                                 # Create an outer division 
@@ -82,16 +82,19 @@ app.layout = html.Div(children=[html.H1('US Domestic Airline Flights Performance
                                     html.Div([
                                         # Create an division for adding dropdown helper text for report type
                                         html.Div(
+                                            [
                                             html.H2('Report Type:', style={'margin-right': '2em'}),
+                                            ]
                                         ),
                                         # TASK2: Add a dropdown
                                         # Enter your code below. Make sure you have correct formatting.
-                                        dcc.Dropdown(
-                                            id='input-type',
-                                            options={'label': 'Yearly Airline Performance Report', 'value': 'OPT1'},{'label': 'Yearly Airline Delay Report', 'value': 'OPT2'},
-                                            placeholder='Select a report type',
-                                            style={'width': '80%','padding': '3px','font-size': '20px','text-align-last':'center'},
-                                            ),
+                                      dcc.Dropdown(id='input-type', 
+                                                    options=[
+                                                            {'label': 'Yearly Airline Performance Report', 'value': 'OPT1'},
+                                                            {'label': 'Yearly Airline Delay Report', 'value': 'OPT2'}
+                                                            ],
+                                                    placeholder='Select a report type',
+                                                    style={'width': '80%','padding': '3px','font-size': '20px','text-align-last':'center'})    
                                     # Place them next to each other using the division style
                                     ], style={'display':'flex'}),
                                     
@@ -110,7 +113,7 @@ app.layout = html.Div(children=[html.H1('US Domestic Airline Flights Performance
                                                      style={'width':'80%', 'padding':'3px', 'font-size': '20px', 'text-align-last' : 'center'}),
                                             # Place them next to each other using the division style
                                             ], style={'display': 'flex'}),  
-                                          ]),
+                                ]),
                                 
                                 # Add Computed graphs
                                 # REVIEW3: Observe how we add an empty division and providing an id that will be updated during callback
@@ -123,29 +126,30 @@ app.layout = html.Div(children=[html.H1('US Domestic Airline Flights Performance
                                 
                                 # TASK3: Add a division with two empty divisions inside. See above disvision for example.
                                 # Enter your code below. Make sure you have correct formatting.
-                               html.Div([
+                                html.Div([
                                         html.Div([ ], id='plot4'),
                                         html.Div([ ], id='plot5')
                                 ], style={'display': 'flex'})
-])
 
 # Callback function definition
 # TASK4: Add 5 ouput components
 # Enter your code below. Make sure you have correct formatting.
-@app.callback([Output(component_id='plot1', component_property='children'),
-               Output(component_id='plot2', component_property='children'),
-               Output(component_id='plot3', component_property='children'),
-               Output(component_id='plot4', component_property='children'),
-               Output(component_id='plot5', component_property='children')],
-               [Input(component_id='input-type', component_property='value'),
-                Input(component_id='input-year', component_property='value')],
-               # REVIEW4: Holding output state till user enters all the form information. In this case, it will be chart type and year
-               [State("plot1", 'children'), State("plot2", "children"),
-                State("plot3", "children"), State("plot4", "children"),
-                State("plot5", "children")
-               ])
+@app.callback(
+    [Output(component_id='plot1', component_property='children'),
+    Output(component_id='plot2', component_property='children'),
+    Output(component_id='plot3', component_property='children'),
+    Output(component_id='plot4', component_property='children'),
+    Output(component_id='plot5', component_property='children')]
+    [Input(component_id='input-type', component_property='value'),
+    Input(component_id='input-year', component_property='value')]
+# REVIEW4: Holding output state till user enters all the form information. In this case, it will be chart type and year
+    [State("plot1", 'children'), State("plot2", "children"),
+    State("plot3", "children"), State("plot4", "children"),
+    State("plot5", "children")])
+    
 # Add computation to callback function and return graph
 def get_graph(chart, year, children1, children2, c3, c4, c5):
+      
         # Select data
         df =  airline_data[airline_data['Year']==int(year)]
        
@@ -171,7 +175,7 @@ def get_graph(chart, year, children1, children2, c3, c4, c5):
                     locationmode = 'USA-states', # Set to plot as US States
                     color_continuous_scale='GnBu',
                     range_color=[0, map_data['Flights'].max()]) 
-                    map_fig.update_layout(
+            map_fig.update_layout(
                     title_text = 'Number of flights from origin state', 
                     geo_scope='usa') # Plot only the USA instead of globe
             
@@ -181,8 +185,8 @@ def get_graph(chart, year, children1, children2, c3, c4, c5):
                       values='Flights',
                       color='Flights',
                       color_continuous_scale='RdBu',
-                      title='Flight count by airline to destination state'
-                )
+                      title='Flight count by airline to destination state')
+            
             
             # REVIEW6: Return dcc.Graph component to the empty division
             return [dcc.Graph(figure=tree_fig), 
